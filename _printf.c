208aa7cd8ee00ed00d0441ca43ce3c940a029ec2
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-01-09 00:45:09 +0400

    python

--- _printf.c
+++ _printf.c
@@ -1,69 +1,65 @@
-#include <stdarg.h>
+[200~#include <stdarg.h>
 #include <stdio.h>
-#include "main.h"  /* You need a header with prototype: int _printf(const char *format, ...); */
+#include "main.h"
 
-/**
- * _printf - prints formatted output to stdout
- * @format: format string
- *
- * Return: number of characters printed (excluding null byte)
- */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0;
-	char *str;
-	char ch;
+    va_list args;
+    int count = 0;
+    char *str;
+    char ch;
 
-	va_start(args, format);
+    va_start(args, format);
 
-	if (!format)
-		return (-1);
+    if (!format)
+        return (-1);
 
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;  /* move past '%' */
-			if (*format == 'c')
-			{
-				ch = va_arg(args, int);
-				putchar(ch);
-				count++;
-			}
-			else if (*format == 's')
-			{
-				str = va_arg(args, char *);
-				if (!str)
-					str = "(null)";
-				while (*str)
-				{
-					putchar(*str);
-					count++;
-					str++;
-				}
-			}
-			else if (*format == '%')
-			{
-				putchar('%');
-				count++;
-			}
-			else
-			{
-				/* Unknown specifier, print literally */
-				putchar('%');
-				putchar(*format);
-				count += 2;
-			}
-		}
-		else
-		{
-			putchar(*format);
-			count++;
-		}
-		format++;
-	}
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            if (*format == '\0')  /* fix for _printf("%") */
+                break;
+            if (*format == 'c')
+            {
+                ch = va_arg(args, int);
+                putchar(ch);
+                count++;
+            }
+            else if (*format == 's')
+            {
+                str = va_arg(args, char *);
+                if (!str)
+                    str = "(null)";
+                while (*str)
+                {
+                    putchar(*str);
+                    count++;
+                    str++;
+                }
+            }
+            else if (*format == '%')
+            {
+                putchar('%');
+                count++;
+            }
+            else
+            {
+                putchar('%');
+                putchar(*format);
+                count += 2;
+            }
+        }
+        else
+        {
+            putchar(*format);
+            count++;
+        }
+        format++;
+    }
 
-	va_end(args);
-	return (count);
+    va_end(args);
+    return (count);
 }
+
